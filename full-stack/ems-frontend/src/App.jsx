b48e28e33ee35e2bbcac2456871c1351b569c10a
hg27haan <hghaan1439@gmail.com>
2024-03-28 12:04:04 +0700

    done Header and Footer

--- full-stack/ems-frontend/src/App.jsx
+++ full-stack/ems-frontend/src/App.jsx
@@ -2,12 +2,16 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import FooterCompoment from './components/FooterCompoment'
+import HeaderCompoment from './components/HeaderCompoment'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
   return (
     <>
+    <HeaderCompoment />
     <ListEmployeeComponent />
+    <FooterCompoment />
     </>
   )
 }
---
+++ full-stack/ems-frontend/src/components/HeaderCompoment.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderCompoment = () => {
+    return (
+    <div>
+        <header>
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="http://www.google.com">Employee Managament System</a>
+            </nav>
+        </header>
+    </div>
+    )
+}
+
+export default HeaderCompoment
