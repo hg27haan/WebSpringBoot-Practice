0f8569207eb59c9faf3ea556233e8e9449a5ff37
hg27haan <hghaan1439@gmail.com>
2024-04-07 11:44:39 +0700

    Adding btn Update, Title and route

--- full-stack/ems-frontend/src/components/ListEmployeeComponent.jsx
+++ full-stack/ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -41,6 +41,10 @@ const ListEmployeeComponent = () => {
     function addNewEmployee(){
         navigator('/add-employee');
     }
+
+    function updateEmployee(id){
+        navigator(`/edit-employee/${id}`)
+    }
     return (
     <div className="container">
         <h2 className= "text-center">List of Employees</h2>
@@ -52,6 +56,7 @@ const ListEmployeeComponent = () => {
                     <th>Employee First Name</th>
                     <th>Employee Last Name</th>
                     <th>Employee Email Id</th>
+                    <th>Actions</th>
                 </tr>
             </thead>
 
@@ -63,6 +68,9 @@ const ListEmployeeComponent = () => {
                             <td>{employee.firstName}</td>
                             <td>{employee.lastName}</td>
                             <td>{employee.email}</td>
+                            <td>
+                                <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
+                            </td>
                         </tr>)
                 }
             </tbody>
